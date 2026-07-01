1f935f0e2dbfa3902a739ad9b65a7a30010bd03e
agayushh <agayush088@gmail.com>
2026-07-01 23:01:13 +0530

    adding multer

--- server/index.ts
+++ server/index.ts
@@ -2,10 +2,19 @@ import express from "express";
 import cors from "cors";
 import multer from "multer";
 
-const upload = multer();
 const app = express();
 app.use(cors());
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, `${uniqueSuffix}-${file.originalname}`);
+  },
+});
+const upload = multer({ storage: storage });
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -14,7 +23,11 @@ app.get("/", (req, res) => {
   });
 });
 
-app.post("/upload/pdf", upload.single("pdf"), (req, res) => {});
+app.post("/upload/pdf", upload.single("pdf"), (req, res) => {
+  return res.json({
+    message: "uploaded",
+  });
+});
 
 app.listen(8000, () => {
   console.log("server is running on port 8000");
